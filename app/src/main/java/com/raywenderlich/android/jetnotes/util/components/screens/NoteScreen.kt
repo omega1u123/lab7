a5a7cdb154ca28f53c7b56c897e5b5b270ec022d
omega1u123 <gvardian2810@gmail.com>
2023-03-15 17:48:59 +0300

    edited NoteScreen.kt

--- app/src/main/java/com/raywenderlich/android/jetnotes/util/components/screens/NoteScreen.kt
+++ app/src/main/java/com/raywenderlich/android/jetnotes/util/components/screens/NoteScreen.kt
@@ -1,23 +1,34 @@
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import com.raywenderlich.android.jetnotes.domain.model.NoteModel
+import com.raywenderlich.android.jetnotes.routing.Screen
+import com.raywenderlich.android.jetnotes.util.components.AppDrawer
 import com.raywenderlich.android.jetnotes.util.components.Note
 import com.raywenderlich.android.jetnotes.util.components.TopAppBar
 import com.raywenderlich.android.jetnotes.viewmodel.MainViewModel
+import kotlinx.coroutines.launch
 
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun NotesScreen(viewModel: MainViewModel) {
     val notes: List<NoteModel> by viewModel
         .notesNotInTrash
         .observeAsState(listOf())
+
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
 
 
     Scaffold(topBar = {
@@ -27,6 +38,17 @@ fun NotesScreen(viewModel: MainViewModel) {
             onIconCLick = {}
         )
     },
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            AppDrawer(
+                currentScreen = Screen.Notes, closeDrawerAction = {
+                    coroutineScope.launch{
+                        scaffoldState.drawerState.close()
+                    }
+                }
+            )
+        },
+
         content = {
             if (notes.isNotEmpty()) {
                 NoteList(
