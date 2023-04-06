164f8d2a77fcb868e7b48605642f1ae8559d9c0f
omega1u123 <gvardian2810@gmail.com>
2023-04-06 21:32:54 +0300

    edited SaveNoteScreen.kt

--- app/src/main/java/com/raywenderlich/android/jetnotes/util/components/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/raywenderlich/android/jetnotes/util/components/screens/SaveNoteScreen.kt
@@ -6,17 +6,27 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import com.raywenderlich.android.jetnotes.R
 import com.raywenderlich.android.jetnotes.domain.model.ColorModel
 import com.raywenderlich.android.jetnotes.util.components.NoteColor
+import com.raywenderlich.android.jetnotes.util.components.TopAppBar
 import com.raywenderlich.android.jetnotes.util.fromHex
 
 
@@ -97,3 +107,65 @@ fun ColorPickerPreview(){
         )
     ){}
 }
+
+@Composable
+private fun SaveNoteTopAppBar(
+    isEditingMode: Boolean,
+    onBackClick: () -> Unit,
+    onSaveNoteClick: () -> Unit,
+    onOpenColorPickerClick: () -> Unit,
+    onDeleteNoteClick: () -> Unit
+){
+
+        androidx.compose.material.TopAppBar(
+            title = {
+                Text(
+                    text = "Save Note",
+                    color = MaterialTheme.colors.onPrimary
+                )
+            },
+        navigationIcon = {
+            IconButton(onClick = onBackClick){}
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "Save Note Button",
+                tint = MaterialTheme.colors.onPrimary
+            )
+        },
+
+        actions = {
+
+            if (isEditingMode){
+                IconButton(
+                    onClick = onDeleteNoteClick
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete Note Button",
+                        tint = MaterialTheme.colors.onPrimary
+                    )
+                }
+            }
+
+          IconButton(onClick = onSaveNoteClick) {
+              Icon(
+                  imageVector = Icons.Default.Check,
+                  tint = MaterialTheme.colors.onPrimary,
+                  contentDescription = "Save Note"
+              )
+          }
+
+          IconButton(onClick = onOpenColorPickerClick) {
+              Icon(
+                  painter = painterResource(
+                        id = R.drawable.ic_baseline_color_lens_24
+                  ),
+                  contentDescription = "Open Color Picker Button",
+                  tint = MaterialTheme.colors.onPrimary
+              )
+          }
+        }
+
+        )
+}
+
